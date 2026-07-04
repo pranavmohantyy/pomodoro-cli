e933249f0b2cfdaf481f7c9bbc1422cfadc85f7d
pranavmohantyy <mohantypranav147@gmail.com>
2026-07-04 14:37:39 -0700

    add session label for tasks

--- timer.py
+++ timer.py
@@ -1,6 +1,7 @@
 import time
 
-def start_timer(duration):
+def start_timer(duration, task_name):
+    print(f'Starting task: {task_name}')
     while duration:
         mins, secs = divmod(duration, 60)
         timer = '{:02d}:{:02d}'.format(mins, secs)
@@ -11,14 +12,15 @@ def start_timer(duration):
 
 
 def pomodoro_cycle():
+    task_name = input('Enter the task name: ')
     for i in range(4):
-        start_timer(25 * 60)
+        start_timer(25 * 60, task_name)
         if i < 3:
             print('Time for a 5-minute break!')
-            start_timer(5 * 60)
+            start_timer(5 * 60, 'Break')
         else:
             print('Time for a 15-minute break!')
-            start_timer(15 * 60)
+            start_timer(15 * 60, 'Long Break')
 
 if __name__ == '__main__':
     pomodoro_cycle()
