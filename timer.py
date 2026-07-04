4a51f361ea1764a0757e7a720d6ed1cf0c518bee
pranavmohantyy <mohantypranav147@gmail.com>
2026-07-04 13:40:04 -0700

    add work/break cycle

--- timer.py
+++ timer.py
@@ -9,5 +9,16 @@ def start_timer(duration):
         duration -= 1
     print('\r00:00')
 
+
+def pomodoro_cycle():
+    for i in range(4):
+        start_timer(25 * 60)
+        if i < 3:
+            print('Time for a 5-minute break!')
+            start_timer(5 * 60)
+        else:
+            print('Time for a 15-minute break!')
+            start_timer(15 * 60)
+
 if __name__ == '__main__':
-    start_timer(25 * 60)
+    pomodoro_cycle()
